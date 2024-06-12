dc38f388ea048f58bd11043f105d2b9b3653ffde
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-06-11 21:08:43 -0500

    Update site

--- js/src/main.js
+++ js/src/main.js
@@ -17,30 +17,40 @@ let d = document;
             const $section = document.getElementById('projects-container');
     
             const $template = `
-                <div class="card">
-                    <div class="card-img">
-                        <img src="${data.image}">
+                <div class="row project">
+                    <div class="col-md-6">
+                        <img src="${data.image}" class="project-img">
                     </div>
                     
-                    <div class="card-content">
-                        <header class="card-header">${data.name}</header>
+                    <div class="col-md-6">
+                        <div class="project-title mt-4 mt-md-0">${data.name}</div>
                         ${Tags(data.tags)}
     
-                        <div class="card-body">
-                            ${data.description}
+                        <div class="project-body">
+                            <div class="mt-4 mt-md-3">
+                                ${data.description}
+                            </div>
                             
     
-                            <div class="card-links">
-                                ${(data.url) ? 
-                                    `<a href="${data.url}" target="_blank">
-                                        <img src="./assets/svg/url.svg" alt="">
-                                    </a>`: ``}
+                            <div class="mt-3">
+                                ${
+                                    data.url
+                                        ? `<a href="${data.url}" target="_blank" class="btn-custom">
+                                        <img src="./assets/svg/url.svg" alt="Daniel Vera" title="${data.name}">
+                                        Ver más
+                                    </a>`
+                                        : ``
+                                }
                                 
-                                ${(data.repository ?
-                                    `<a href="${data.repository}"
-                                        target="_blank">
-                                        <img src="./assets/svg/github.svg" alt="">
-                                    </a>` : ``)}
+                                ${
+                                    data.repository
+                                        ? `<a href="${data.repository}"
+                                        target="_blank" class="btn-custom">
+                                        <img src="./assets/svg/github.svg" alt="Daniel Vera" title="${data.name}">
+                                        Code
+                                    </a>`
+                                        : ``
+                                }
                             </div>
                         </div>
                     </div>
